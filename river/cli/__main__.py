22c6759ba163e03be22021a03bac604dcf39f2e9
Nico Stefani <nicolas471@gmail.com>
2024-06-18 17:46:59 -0300

    Implement ruff linter and formatter

--- river/cli/__main__.py
+++ river/cli/__main__.py
@@ -1,15 +1,16 @@
 import click
 from river.core.utils import say_hello_to
 
+
 @click.group
 def cli():
-    pass
+	pass
 
 
 @cli.command
 def hello():
-    click.echo(say_hello_to('RIVeR'))
+	click.echo(say_hello_to("RIVeR"))
 
 
-if __name__ == '__main__':
-    cli()
+if __name__ == "__main__":
+	cli()
